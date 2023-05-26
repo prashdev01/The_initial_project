1060cd7a03b6cf1906d5074505603037042fafac
prashdev01 <prashantwadhave5@gmail.com>
2023-05-26 10:57:53 +0530

    added type js animation

--- app.py
+++ app.py
@@ -71,10 +71,6 @@ def index():
                 mydict = {"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
                           "Comment": custComment}
                 reviews.append(mydict)
-            client = pymongo.MongoClient("mongodb+srv://RE_SCRAP:Prashant@cluster0.34xptzg.mongodb.net/?retryWrites=true&w=majority")
-            db = client['review_scrap']
-            review_col = db['review_scrap_data']
-            review_col.insert_many(reviews)
             return render_template('results.html', reviews=reviews[0:(len(reviews)-1)])
         except Exception as e:
             print('The Exception message is: ',e)
